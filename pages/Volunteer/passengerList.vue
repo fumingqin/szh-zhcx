4d8dd2ceb41a0eecbb102ca6effb8cd8afc2f15f
Shiyj <844349552@qq.com>
2020-07-03 15:01:42 +0800

    志愿者-扫码输入功能

--- pages/Volunteer/passengerList.vue
+++ pages/Volunteer/passengerList.vue
@@ -9,12 +9,30 @@
 			
 		</view>
 		<view class="button-block">
-			<button @click="manual" class="add-button">手动输入</button>
-			<button @click="scan" class="add-button">扫码输入</button>
+			<button @click="manual" class="add-button" style="font-size: 30rpx;">手动输入</button>
+			<button @click="scan" class="add-button" style="font-size: 30rpx;">扫码输入</button>
+			<button @click="finish" class="add-button" style="font-size: 30rpx;">完成</button>
 		</view>
 		
-		<uni-popup ref="passenger" type="dialog">
-			<uni-popup-dialog type="info" mode="input" :before-close='false' :value='popupValue' @close="popupClose" @confirm="popupConfirm"></uni-popup-dialog>
+		<uni-popup ref="addPassenger" type="center">
+			<view class="popupBlock">
+				<view class="popupTitle">
+					<text>请输入编号</text>
+				</view>
+				<view class="popupInputBlock">
+					<view class="popupInput">
+						<input v-model="popupValue" placeholder="请输入编号" />
+					</view>
+				</view>
+				<view class="popupButtonBlock">
+					<view class="popupButtonItem itemRight" @click="popupClose">
+						<text>取消</text>
+					</view>
+					<view class="popupButtonItem" @click="popupConfirm">
+						<text class="popupOkColor">确定</text>
+					</view>
+				</view>
+			</view>
 		</uni-popup>
 	</view>
 </template>
@@ -22,16 +40,12 @@
 <script>
 	import uniSwipeAction from '@/components/Volunteer/uni-swipe-action/uni-swipe-action.vue';
 	import uniSwipeActionItem from '@/components/Volunteer/uni-swipe-action-item/uni-swipe-action-item.vue';
-	import uniPopup from '@/components/Volunteer/uni-popup/uni-popup.vue'
-	import uniPopupMessage from '@/components/Volunteer/uni-popup/uni-popup-message.vue'
-	import uniPopupDialog from '@/components/Volunteer/uni-popup/uni-popup-dialog.vue'
+	import uniPopup from '@/components/uni-popup/uni-popup.vue'
 	export default {
 		components: {
 		        uniSwipeAction,
 		        uniSwipeActionItem,
-				uniPopup,
-				uniPopupMessage,
-				uniPopupDialog,
+				uniPopup, 
 		},
 		data() {
 			return {
@@ -49,11 +63,9 @@
 						}
 					}
 				],
-				passengers:[
-					'123',
-					'123',
-				],
-				popupValue:''
+				passengers:[],
+				popupValue:'',
+				index:-1,
 			}
 		},
 		methods: {
@@ -64,15 +76,25 @@
 				});
 			},
 			openPopup:function(){
-				this.$refs.passenger.open();
+				let that = this;
+				that.popupValue = '';
+				that.index = -1;
+				that.$refs.addPassenger.open();
+			},
+			closePopup:function(){
+				let that = this;
+				that.$refs.addPassenger.close();
 			},
 			//左滑编辑打开事件
 			openPopupEdit:function(item,index){
-				this.$refs.passenger.open();
+				let that = this;
+				that.showLoading();
+				that.popupValue = item;
+				that.index = index;
+				that.$refs.addPassenger.open();
+				uni.hideLoading();
 			},
-			closePopup:function(){
-				this.$refs.passenger.close();
-			},
+			
 			changeItem:function(e,item,index){
 				
 			},
@@ -87,7 +109,6 @@
 			//手动输入
 			manual:function(){
 				let that = this;
-				that.popupValue = '';
 				that.openPopup();
 			},
 			//扫码输入
@@ -97,9 +118,26 @@
 				uni.scanCode({
 					success:function(res){
 						that.passengers.push(res.result);
+					},
+					fail:function(){
 						uni.hideLoading();
-					}
+					},
+					complete:function(){
+						uni.hideLoading();
+					},
 				})
+			},
+			finish:function(){
+				let that = this;
+				if(that.passengers.length > 0){
+					uni.setStorageSync('passengers',that.passengers);
+					uni.navigateBack();
+				}else{
+					uni.showToast({
+						title:'请至少录入一个'
+					});
+				}
+				
 			},
 			//左滑删除按钮
 			delete:function(index){
@@ -112,21 +150,27 @@
 				that.openPopupEdit(item,index);
 			},
 			//弹出层取消按钮
-			popupClose:function(done){
+			popupClose:function(){
 				let that = this;
-				done();
+				that.closePopup();
 			},
 			//弹出层确定按钮
-			popupConfirm:function(done,value){
+			popupConfirm:function(){
 				let that = this;
-				that.passengers.push(value);
-				done();
+				if(that.index === -1){
+					that.passengers.push(that.popupValue);
+				}else if(that.index > -1){
+					that.passengers[that.index] = that.popupValue;
+					console.log(that.passengers);
+					that.$forceUpdate();
+				}
+				that.closePopup();
 			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	
 	.button-block{
 		position: fixed;
@@ -139,13 +183,12 @@
 	}
 	
 	.add-button{
-		width: 300rpx;
+		width: 200rpx;
 		margin: 0 auto;
 		color: #FFFFFF;
 		background:linear-gradient(270deg,rgba(94,109,255,1),rgba(73,152,251,1));
 		box-shadow:0px 7px 38px 8px rgba(70,103,252,0.15);
-	}
-	
+	}	
 	.cont {
 		flex: 1;
 		height: 45px;
@@ -157,5 +200,43 @@
 		border-bottom-color: #F5F5F5;
 		border-bottom-width: 1px;
 		border-bottom-style: solid;
+	}
+	.popupBlock {
+		width: 550rpx;
+		padding: 30rpx 0;
+		background-color: #FFFFFF;
+		border-radius: 12rpx;
+	}
+	.popupTitle{
+		text-align: center;
+		margin-bottom: 30rpx;
+	}
+	.popupTitle text{
+		color: $uni-text-color-grey;
+	}
+	.popupInputBlock{
+		padding:0 30rpx 30rpx 30rpx ;
+		margin-bottom: 30rpx;
+		border-bottom: 1px solid #E6E6E6;
+	}
+	.popupInput{
+		width: 450rpx;
+		margin: 0 auto;
+	}
+	.popupButtonBlock{
+		display: flex;
+		flex-direction: row;
+		width: 550rpx;
+		margin: 0 auto;
+	}
+	.popupButtonItem{
+		text-align: center;
+		width: 275rpx;
+	}
+	.popupOkColor{
+		color: $uni-color-primary;
+	}
+	.itemRight{
+		border-right: 1px solid #E6E6E6;
 	}
 </style>
