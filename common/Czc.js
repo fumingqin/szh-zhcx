d8da6d9229e17b9cf14dcbd6df8996af338a148d
zhangzx <1256267790@qq.com>
2020-09-26 11:08:59 +0800

    11

--- common/Czc.js
+++ common/Czc.js
@@ -2,7 +2,7 @@
 // 示例
 
 //接口域名
-const Url = 'https://36.148.9.1:10022';//许接口地址
+const Url = 'https://szy.qzcln.com';//许接口地址
 const Url1 = 'https://szh.ipin8.com:3507';//叶接口地址
 //接口对象
 const Interface = {
